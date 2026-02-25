93adbcc3ab1cd64e4eaacd7f5a2ee1611b36668f
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-02-25 09:53:26 -0300

    Add files via upload

--- jokenpo.js
+++ jokenpo.js
@@ -11,22 +11,36 @@ function jogar(){
 
         switch(sorteio){
             case 0 : 
-                document.getElementById("pc").src="pcpedra.png";
+                document.getElementById('pc').src="pcpedra.png";
                 break
             
             case 1 : 
-                document.getElementById("pc").src="pcpapel.png";
+                document.getElementById('pc').src="pcpapel.png";
                 break
             
             case 2 : 
-                document.getElementById("pc").src="pctesoura.png";
+                document.getElementById('pc').src="pctesoura.png";
                 break
             }
             if ((document.getElementById('pedra').checked == true && sorteio ==0)
                 ||(document.getElementById('papel').checked == true && sorteio ==1)
-            ||(document.getElementById('tesoura').checked == true && sorteio ==2)||){
-
+            ||(document.getElementById('tesoura').checked == true && sorteio ==2))
+            
+            {
+                    document.getElementById('placar').innerHTML="Empate";
+            }
+            else if((document.getElementById('pedra').checked == true && sorteio == 2)||
+            (document.getElementById('papel').checked == true && sorteio == 0)||
+            (document.getElementById('tesoura').checked == true && sorteio == 1))
+            {
+                    document.getElementById('placar').inertHTML = "Você venceu!!";
+            }
+            else{
+                    document.getElementById('placar').innerHTML="Computador venceu!";
             }
         }
-
     }
+function resetar(){
+    document.getElementById('pc').src="pc.png";
+    document.getElementById('placar').innerHTML="";
+}
